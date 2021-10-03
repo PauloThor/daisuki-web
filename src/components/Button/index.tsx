db746711f5cf087aaa5ff2a9cbd37a4e9ed838f8
Thaina Ferreira <thainafecosta@gmail.com>
2021-10-03 20:36:02 -0300

    feat: button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,30 @@
+import { ButtonStyle } from "./style";
+
+interface ButtonProps {
+  text: string;
+  color?: string;
+  margin?: string;
+  type?: "submit" | "button" | "reset" | undefined;
+  handleClick?: () => void;
+}
+
+const Button = ({
+  text,
+  color,
+  margin,
+  type = "submit",
+  handleClick,
+}: ButtonProps) => {
+  return (
+    <ButtonStyle
+      type={!handleClick ? "submit" : type}
+      onClick={handleClick}
+      color={color}
+      margin={margin}
+    >
+      {text}
+    </ButtonStyle>
+  );
+};
+
+export default Button;
