a5c46ad723e10e4dda09ec3014cea876e4c0ac48
Laiane Suzart <laiane@pm.me>
2021-10-02 21:06:41 -0300

    feat: not found page with redirect

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,32 +1,39 @@
 import { Link } from "react-router-dom";
 import Lottie from "react-lottie";
-import sasuke from "../../assets/lottie/sasuke.json"
-import {FullContainer, TextWrapper, TextContainer, AnimationWrapper, LottieWrapper} from "./styles"
+import sasuke from "../../assets/lottie/sasuke.json";
+import {
+  FullContainer,
+  TextWrapper,
+  TextContainer,
+  AnimationWrapper,
+  LottieWrapper,
+} from "./styles";
 
 const NotFound = () => {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: sasuke,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice",
-        },
-      };
-    return (
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: sasuke,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
     <FullContainer>
-        <TextWrapper>
+      <TextWrapper>
         <TextContainer>
-            <p>Ahn? Esse não é o caminho certo.</p>
-            <Link to="/">Vamos voltar~</Link>
+          <p>Ahn? Esse não é o caminho certo.</p>
+          <Link to="/">Vamos voltar~</Link>
         </TextContainer>
-        </TextWrapper>
-        <AnimationWrapper>
-            <LottieWrapper>
-                <Lottie options={defaultOptions}/>
-            </LottieWrapper>
-        </AnimationWrapper>
+      </TextWrapper>
+      <AnimationWrapper>
+        <p>404</p>
+        <LottieWrapper>
+          <Lottie options={defaultOptions} />
+        </LottieWrapper>
+      </AnimationWrapper>
     </FullContainer>
-        )
-}
+  );
+};
 
 export default NotFound;
