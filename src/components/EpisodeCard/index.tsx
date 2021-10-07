b4208b6e3ef4b90ba7bb176d16c8d2c9c8be3a1c
Laiane Suzart <laiane@pm.me>
2021-10-07 11:45:32 -0300

    chore: add atributes

--- src/components/EpisodeCard/index.tsx
+++ src/components/EpisodeCard/index.tsx
@@ -1,8 +1,8 @@
 import { Episode } from "../../model/episode"
 
 const EpisodeCard = (episode: Episode) => {
-    const {image_url, } = episode
-    
+    const {image_url, anime_name, episode_number, created_at} = episode
+
     return (
         <div></div>
     )
--- src/model/episode.ts
+++ src/model/episode.ts
@@ -1,10 +1,11 @@
+import { Anime } from "./anime";
+
 export interface Episode {
   id?: number;
-  anime_name?: string;
-  episode_number: number;
-  image_url: string;
-  video_url: string;
-  views: number;
-  anime_id: number;
-  created_at: string;
+  episode_number?: number;
+  image_url?: string;
+  video_url?: string;
+  views?: number;
+  anime?: Anime;
+  created_at?: string;
 }
