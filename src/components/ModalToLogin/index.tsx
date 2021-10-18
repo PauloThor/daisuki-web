f20b6fe245500bff3bd420f5d10f8f9a7e930244
Matheus Paiva <matheeus_paiva@hotmail.com>
2021-10-18 19:11:23 -0300

    feat: Modal login

--- src/components/ModalToLogin/index.tsx
+++ src/components/ModalToLogin/index.tsx
@@ -1,20 +1,69 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { useHistory } from "react-router";
+import { useUser } from "../../hooks/User";
+import { InputTypes } from "../../model/enums/input-types";
+import { CheckboxStyled } from "../../pages/Admin/styles";
+import SchemaUtils from "../../shared/util/schema-utils";
 import Button from "../Button";
-import { Content, StyledModal } from "./styles";
+import InputText from "../InputText";
+import {
+  CheckboxContainer,
+  Container,
+  ContainerButtons,
+  Content,
+  Form,
+  FormContainer,
+  StyledLink,
+  StyledModal,
+  Subtitle,
+} from "./styles";
 
-interface ModalSynopsisProps {
+interface ModalToLoginProps {
   isModalToLoginVisible: boolean;
   handleModalToLogin: () => void;
+}
+
+interface FormInput {
+  email: string;
+  password: string;
 }
 
 export const ModalToLogin = ({
   isModalToLoginVisible,
   handleModalToLogin,
-}: ModalSynopsisProps) => {
+}: ModalToLoginProps) => {
   const history = useHistory();
-  const handleToLogin = () => {
-    history.push("/login");
+  const [shouldRemember, setShouldRemember] = useState<boolean>(false);
+  const { login } = useUser();
+
+  const handleRemember = () => setShouldRemember(!shouldRemember);
+
+  const methods = useForm({
+    resolver: yupResolver(SchemaUtils.login()),
+    mode: "all",
+  });
+
+  const onSubmit = (data: FormInput) => {
+    login({ ...data, remindMe: shouldRemember }, history);
   };
+
+  const inputList = [
+    {
+      name: "email",
+      placeholder: "exemplo@mail.com",
+      label: "E-mail*",
+      type: InputTypes.EMAIL,
+    },
+    {
+      name: "password",
+      placeholder: "Senha",
+      label: "Senha*",
+      type: InputTypes.PASSWORD,
+    },
+  ];
+
   return (
     <StyledModal
       title="Login"
@@ -22,15 +71,49 @@ export const ModalToLogin = ({
       onCancel={handleModalToLogin}
     >
       <Content>
-        <p>Faça o login para avaliar o anime!</p>
-        <div>
-          <Button
-            text="Cancelar"
-            margin="0px"
-            handleClick={handleModalToLogin}
-          />
-          <Button text="Fazer login" margin="0px" handleClick={handleToLogin} />
-        </div>
+        <Container>
+          <FormContainer>
+            <FormProvider {...methods}>
+              <Form onSubmit={methods.handleSubmit(onSubmit)}>
+                {inputList.map((input, index) => (
+                  <InputText
+                    key={`${input.name}-${index}`}
+                    name={input.name}
+                    placeholder={input.placeholder}
+                    label={input.label}
+                    type={input?.type ?? ""}
+                    autofocus={index === 0}
+                  />
+                ))}
+                <CheckboxContainer>
+                  <CheckboxStyled
+                    onChange={handleRemember}
+                    checked={shouldRemember}
+                  >
+                    Lembrar de mim
+                  </CheckboxStyled>
+                </CheckboxContainer>
+                <ContainerButtons>
+                  <Button
+                    text="Cancelar"
+                    margin="0px"
+                    handleClick={handleModalToLogin}
+                  />
+                  <Button text="Fazer login" margin="0px" type="submit" />
+                </ContainerButtons>
+                <Subtitle>
+                  <StyledLink to="/recover-password">
+                    Esqueceu a senha?
+                  </StyledLink>
+                </Subtitle>
+                <Subtitle>
+                  Não é cadastrado?
+                  <StyledLink to="/register"> Criar conta.</StyledLink>
+                </Subtitle>
+              </Form>
+            </FormProvider>
+          </FormContainer>
+        </Container>
       </Content>
     </StyledModal>
   );
--- src/components/ModalToLogin/styles.ts
+++ src/components/ModalToLogin/styles.ts
@@ -1,17 +1,21 @@
 import styled from "styled-components";
 import { Modal } from "antd";
 import { Color } from "../../model/enums/theme-colors";
+import { Link } from "react-router-dom";
+import InputText from "../InputText";
 
 export const Content = styled.div`
   overflow: auto;
-  max-height: 40vh;
+  max-height: 60vh;
+  display: flex;
+  flex-direction: column;
 
   p {
     color: white;
     font-size: 1rem;
   }
 
-  div {
+  /* div {
     margin-top: 1rem;
     display: flex;
     justify-content: space-around;
@@ -24,7 +28,7 @@ export const Content = styled.div`
     button + button {
       background-color: ${Color.HIGHLIGHT_DARK};
     }
-  }
+  } */
 `;
 
 export const StyledModal = styled(Modal)`
@@ -50,5 +54,109 @@ export const StyledModal = styled(Modal)`
 
   .ant-modal-footer {
     display: none;
+  }
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  justify-content: start;
+  /* align-items: center; */
+
+  img {
+    width: 100px;
+    height: 50px;
+  }
+
+  @media (min-width: 768px) {
+    justify-content: start;
+  }
+
+  @media (min-width: 1980px) {
+    padding-left: 100px;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  /* flex-direction: column; */
+  align-items: center;
+  overflow: hidden;
+`;
+
+export const FormContainer = styled.div`
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+  }
+`;
+
+export const Form = styled.form`
+  background-color: ${Color.MAIN};
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  /* flex-wrap: wrap; */
+  justify-content: center;
+  width: 20rem;
+
+  @media (min-width: 768px) {
+    /* width: 350px; */
+  }
+`;
+
+export const Subtitle = styled.p`
+  width: 100%;
+  color: ${Color.TEXT_SECONDARY};
+  padding: 0.5rem 1rem;
+  font-size: 0.8rem;
+  text-align: right;
+`;
+
+export const StyledLink = styled(Link)`
+  font-weight: 700;
+  color: ${Color.TEXT_SECONDARY};
+
+  &:hover,
+  &:focus {
+    color: ${Color.TEXT_MAIN};
+    transition: 0.5s;
+  }
+`;
+
+export const CheckboxContainer = styled.div`
+  width: 100%;
+  display: flex;
+
+  span {
+    font-size: 0.9rem;
+  }
+`;
+
+export const StyledInputText = styled(InputText)`
+  .sc-fodVek {
+    flex-direction: column;
+  }
+
+  div {
+    flex-direction: column;
+  }
+`;
+
+export const ContainerButtons = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  justify-content: space-around;
+  gap: 1rem;
+
+  button {
+    background: ${Color.MAIN_LIGHT};
+    color: white;
+  }
+  button + button {
+    background-color: ${Color.HIGHLIGHT_DARK};
   }
 `;
